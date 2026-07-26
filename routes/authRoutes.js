5a2f45e7fbc6b736f97f244db46603c8fab4b3de
lintrix <lintrix@Lintrix.lintrix.com>
2026-07-26 10:17:05 +0300

    Migrating to jwt after login from passport

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,15 @@ const authenticate = require('../middleware/jwtAuth');
 const router = express.Router();
 
 module.exports = (authController) => {
+
+  console.log({
+    authenticate: typeof authenticate,
+    googleAuth: typeof authController.googleAuth,
+    googleCallback: typeof authController.googleCallback,
+    authFailure: typeof authController.authFailure,
+    logout: typeof authController.logout,
+    getAuthStatus: typeof authController.getAuthStatus,
+  });
   // Google OAuth routes
   router.get('/google', authController.googleAuth);
   router.get('/google/callback', authController.googleCallback);
